7a019dcdb1f1d17d22b5ef27b99d22d5937476e6
Nick Olds <nolds9@yahoo.com>
2016-11-17 10:04:08 -0500

    removes boilerplate

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>Hello World</p>
+      </div>
+    )
+  }
+}
+
+export default App;
